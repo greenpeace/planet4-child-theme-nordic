ebbb565d3e4b94bbf37fb2eb59cfbd8b5e123c8c
skoleva <svilena_koleva@yahoo.com>
2020-08-20 15:39:30 +0200

    [HOLD] Version up

--- functions.php
+++ functions.php
@@ -21,7 +21,7 @@ function enqueue_child_scripts()  {
     wp_enqueue_script('popper');
     wp_register_script('bootstrap', 'https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js', [ 'jquery', 'popper' ], '4.4.1', true);
     wp_enqueue_script('bootstrap');
-    wp_register_script('child-js', get_stylesheet_directory_uri() . '/assets/build/index.js', ['jquery'], '0.2', true);
+    wp_register_script('child-js', get_stylesheet_directory_uri() . '/assets/build/index.js', ['jquery'], '0.3', true);
     wp_enqueue_script('child-js');
 
 }
