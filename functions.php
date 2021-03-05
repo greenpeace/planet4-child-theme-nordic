6212c9552a74fd0542dc0c215aa07867bfe7a1af
skoleva <svilena_koleva@yahoo.com>
2021-03-05 15:03:30 +0100

    Fixed the breaking border under navs

--- functions.php
+++ functions.php
@@ -44,8 +44,8 @@
 add_action('wp_enqueue_scripts', 'enqueue_child_styles');
 function enqueue_child_styles() {
     $css_creation = filectime(get_stylesheet_directory() . '/assets/build/style.min.css');
-    wp_enqueue_style('bootstrap', 'https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.min.css', array(), '5.0.0');
-    wp_enqueue_style('child-style', get_stylesheet_directory_uri() . '/assets/build/style.min.css', ['bootstrap','parent-style'], $css_creation);
+    // wp_enqueue_style('bootstrap', 'https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.min.css', array(), '5.0.0');
+    wp_enqueue_style('child-style', get_stylesheet_directory_uri() . '/assets/build/style.min.css', ['parent-style'], $css_creation);
 
 }
 
