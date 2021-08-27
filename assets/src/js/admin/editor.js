1730e17f354aa1c7f1142116ee685b03f0f7d317
svilena <svilena_koleva@yahoo.com>
2021-08-27 11:04:02 +0200

    ECI madnezz

--- assets/src/js/admin/editor.js
+++ assets/src/js/admin/editor.js
@@ -1,6 +1,59 @@
 console.log('Adding the ECI script here..');
-// Fold the form before signup
+window.addEventListener('DOMContentLoaded', (event) => {
+  let eciForm = document.getElementById('proca');
+
+  if(eciForm){
+    eciForm = true;
+    console.log('ECI LOADED..');
+  }else{
+    eciForm = false;
+    console.log('NO ECI..');
+  }
+
+  switch(eciForm){
+    case true:
+      console.log('ECI SWITCH TRUE..');
+      jQuery(function() {
+        // Add petition form styling to ECI form (can do this using CSS instead)
+        jQuery('.eci-form-wrapper').css({
+          "padding": "1.5rem",
+          "padding-bottom": "2rem"
+        });
+
+        // Hide your normal petition form (can do this using CSS instead)
+        jQuery('.leads-form__form__container').hide();
+
+        // Hide your normal petition text (can do this using CSS instead)
+        // jQuery('.under-85, .description').hide();
+        //removed this one bc of the heading class being dynamic
+
+        jQuery('.leads-form__content h2').hide();
+        jQuery('.leads-form__content .description').hide();
+
+        // Move ECI Form to inside normal form
+        jQuery('.leads-form__form').prepend(jQuery('.eci-form-wrapper'));
+
+        // Move ECI text to inside normal tet
+        jQuery('.leads-form__content').prepend(jQuery('.eci-text-wrapper'));
+
+        // Show ECI Form and Text
+        jQuery('.eci-form-wrapper, .eci-text-wrapper').show();
+
+      });
+      break;
+    case false:
+      console.log('ECI SWITCH FALSE..');
+      jQuery('.leads-form__form__container, .leads-form__content h2, .leads-form__content .description').show();
+      break;
+    default:
+    console.log('DEFAULT SWITCH CASE..');
+      // Show your normal petition form & text
+      jQuery('.leads-form__form__container, .leads-form__content h2, .leads-form__content .description').show();
+  }
+});
+
 window.addEventListener('proca', (event) => {
+   //fold b4 signup
   let child1 = jQuery('form#proca-register :nth-child(4)');
   let child2 = jQuery('form#proca-register :nth-child(5)');
   let child3 = jQuery('form#proca-register :nth-child(6)');
@@ -10,9 +63,8 @@ window.addEventListener('proca', (event) => {
     window.scroll({
       behavior: 'smooth'
     });
+    console.log("folded");
   });
-  jQuery(child4).css({"padding": "1.5rem 0"});
-  console.log("folded");
 
   //Unfold the form when clicked on fname
   jQuery('#proca_firstname').on('click',  function() {
@@ -22,70 +74,40 @@ window.addEventListener('proca', (event) => {
           left: 0,
           behavior: 'smooth'
       });
+      console.log("unfolded");
+    });
+  });
+
+  jQuery(child4).css({"padding": "1.5rem 0"});
+
+});
+
+
+
+// Listen to the "ECI Completed Event"
+window.addEventListener("eci:complete", function (e) {
+  window.dataLayer.push({
+    'event': 'petitionSignup ECI'
+  });
+
+  jQuery('.eci-form-wrapper, .eci-text-wrapper').hide();
+
+  // Show your normal petition form & text
+  jQuery('.leads-form__form__container, .leads-form__content h2, .leads-form__content .description').fadeIn(1400, function() {
+    window.scroll({
+        top: 0,
+        left: 0,
+        behavior: 'smooth'
     });
   });
 });
 
-// the ECI widget
+
+// the ECI counter widget
 window.addEventListener("proca", function (e) {
 	if (e.detail.message == "count") {
 		var counter = document.querySelector('.eci-counter');
 		counter.innerText = e.detail.value;
 	}
 });
-
-let eciForm = jQuery('#proca');
-
-if(eciForm){
-  console.log('ECI exist..');
-  jQuery(function() {
-    // Add petition form styling to ECI form (can do this using CSS instead)
-    jQuery('.eci-form-wrapper').css({
-      "padding": "1.5rem",
-      "padding-bottom": "2rem"
-    });
-
-    // Hide your normal petition form (can do this using CSS instead)
-    jQuery('.leads-form__form__container').hide();
-
-    // Hide your normal petition text (can do this using CSS instead)
-    // jQuery('.under-85, .description').hide();
-    //removed this one bc of the heading class being dynamic
-
-    jQuery('.leads-form__content h2').hide();
-    jQuery('.description').hide();
-
-    // Move ECI Form to inside normal form
-    jQuery('.leads-form__form').prepend(jQuery('.eci-form-wrapper'));
-
-    // Move ECI text to inside normal tet
-    jQuery('.leads-form__content').prepend(jQuery('.eci-text-wrapper'));
-
-    // Show ECI Form and Text
-    jQuery('.eci-form-wrapper, .eci-text-wrapper').show();
-
-    // Listen to the "ECI Completed Event"
-    window.addEventListener("eci:complete", function (e) {
-      window.dataLayer.push({
-        'event': 'petitionSignup ECI'
-      });
-
-      // Hide the ECI form & text
-      jQuery('.eci-form-wrapper, .eci-text-wrapper').hide();
-
-      // Show your normal petition form & text
-      jQuery('.leads-form__form__container, .leads-form__content h2, .description').fadeIn(1400, function() {
-        window.scroll({
-            top: 0,
-            left: 0,
-            behavior: 'smooth'
-        });
-      });
-      // jQuery('.leads-form__form__container, .under-85, .description').show();
-    });
-  });
-}else{
-  console.log('ECI NOT exist..');
-}
-
 
