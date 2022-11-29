86d7e35a3c8d623e38ef20161e2c67fca8097ac8
svilena <svilena_koleva@yahoo.com>
2022-11-29 12:22:13 +0100

    feat: Tracking patterns
    Tracking new pattern elements reusable nav links,  page header CTA, deep-dive pattern and high-level  quick links and page type (HL & DD)

--- assets/src/js/admin/tracking.js
+++ assets/src/js/admin/tracking.js
@@ -1,5 +1,4 @@
-
-//tracking the sub-menu links clicks
+//tracking the sub-menu block link clicks
 window.addEventListener('DOMContentLoaded', (event) => {
   jQuery('.submenu-link').on('click', function(e) {
     let submenuLinkHref = jQuery(this).attr('href');
@@ -9,4 +8,71 @@ window.addEventListener('DOMContentLoaded', (event) => {
       'event': 'menuClick'
     });
   });
+
+  //custom tracking for P4 new IA&NAV
+  //page types
+  let highLevel = document.querySelectorAll('.is-pattern-p4-high-level-topic-pattern-layout');
+  let deepDive = document.querySelectorAll('.is-pattern-p4-deep-dive-topic-pattern-layout');
+  if (highLevel) {
+    window.dataLayer.push({'pageType': 'High-level Topic'});
+    //console.log(dataLayer);
+  } else if (deepDive) {
+    window.dataLayer.push({'pageType': 'Deep-dive Topic'});
+    //console.log(dataLayer);
+  } else {
+    //console.log(dataLayer);
+  }
+
+  //page header clicks
+  const cta = document.querySelectorAll(".is-pattern-p4-page-header");
+  for (let  li = 0 ; li < cta.length; li++) {
+      cta[li].addEventListener('click' , function(e){
+      window.dataLayer.push({
+        'eventCategory': 'Header',
+        'eventAction': 'Call to Action',
+        'event': 'navClick'
+      });
+      }, true );
+  }
+
+  //secondary nav clicks
+  const links = document.querySelectorAll(".nav-link");
+  for (let  li = 0 ; li < links.length; li++) {
+      links[li].addEventListener('click' , function(e){
+
+      let shorthdl = links[li].textContent;
+      window.dataLayer.push({
+          'eventCategory': 'Sub Menu',
+          'eventAction': shorthdl,
+          'event': 'navClick'
+      });
+      }, true );
+  }
+
+  //other deep-dive topics clicks
+  const ddlinks = document.querySelectorAll(".is-pattern-p4-deep-dive");
+  for (let  li = 0 ; li < ddlinks.length; li++) {
+      ddlinks[li].addEventListener('click' , function(e){
+       window.dataLayer.push({
+        'eventCategory': 'Read More Topics',
+        'eventAction': 'Deep-Dive Topics',
+        'event': 'navClick'
+      });
+      }, true );
+  }
+
+  //other high-level topics clicks
+  const hllinks = document.querySelectorAll(".is-pattern-p4-quick-links");
+  for (let  li = 0 ; li < hllinks.length; li++) {
+      hllinks[li].addEventListener('click' , function(e){
+        window.dataLayer.push({
+        'eventCategory': 'Read More Topics',
+        'eventAction': 'High Level Topics',
+        'event': 'navClick'
+      });
+      }, true );
+  }
+
 });
+
+
