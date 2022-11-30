3ba0c82ce6a6dadbb39b77bd9f4d05a164684e32
svilena <svilena_koleva@yahoo.com>
2022-11-30 15:50:19 +0100

    JS version up

--- functions.php
+++ functions.php
@@ -39,7 +39,7 @@ function enqueue_child_scripts()
     // wp_enqueue_script('popper');
     // wp_register_script('bootstrap', 'https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/js/bootstrap.bundle.min.js', ['bootstrap'], '5.0.0', true);
     // wp_enqueue_script('bootstrap');
-    wp_register_script('child-js', get_stylesheet_directory_uri() . '/assets/build/index.js', ['jquery'], '0.21', true);
+    wp_register_script('child-js', get_stylesheet_directory_uri() . '/assets/build/index.js', ['jquery'], '0.22', true);
     wp_enqueue_script('child-js');
 }
 add_action('wp_enqueue_scripts', 'enqueue_child_scripts');
