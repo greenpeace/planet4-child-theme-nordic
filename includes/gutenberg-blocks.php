fd1671423bcd4cccecfe1b5f41a9ac4fbc49857d
svilena <svilena_koleva@yahoo.com>
2023-02-03 13:52:38 +0100

    fix: p4 custom templates missing from the listed patterns

--- includes/gutenberg-blocks.php
+++ includes/gutenberg-blocks.php
@@ -4,17 +4,13 @@
  *
  * @since 5.8.0
  *
- * @param WP_Block_Editor_Context $block_editor_context The current block editor context.
+ * @param WP_Block_context $block_context The current block editor context.
  *
  * @return bool|string[] Array of block type slugs, or boolean to enable/disable all.
  *
  */
 
-if ( ! function_exists( 'p4_child_theme_gpn_whitelist_blocks' ) ) {
-	//add_filter( 'allowed_block_types_all', 'p4_child_theme_gpn_whitelist_blocks', 10, 2 );
-
-	add_filter( 'allowed_block_types_all', 'p4_child_theme_gpn_whitelist_blocks', 10, 2 );
-
+if ( ! function_exists( 'p4_child_theme_gpn_allowed_list' ) ) {
 	/**
 	 * Whitelists Gutenberg Blocks
 	 *
@@ -24,8 +20,8 @@ if ( ! function_exists( 'p4_child_theme_gpn_whitelist_blocks' ) ) {
 	 * @return array
 	 */
 
-	function p4_child_theme_gpn_whitelist_blocks( $allowed_block_types, $editor_context ) {
-		$allowed_blocks_general = array(
+	function p4_child_theme_gpn_allowed_list( $allowed_block_types, $block_editor_context ) {
+		$allowed_blocks_general = [
 			'core/block',
 			'core/paragraph',
 			'core/heading',
@@ -43,10 +39,10 @@ if ( ! function_exists( 'p4_child_theme_gpn_whitelist_blocks' ) ) {
 			'core/group',
 			'core/columns',
 			'core/column',
-			'core/cover',
+			'core/cover', //extra
 			'core/embed',
 			'core/media-text',
-			'core/post-content',
+			'core/post-content', //extra
 			'core-embed/twitter',
 			'core-embed/youtube',
 			'core-embed/facebook',
@@ -71,11 +67,22 @@ if ( ! function_exists( 'p4_child_theme_gpn_whitelist_blocks' ) ) {
 			'core-embed/speaker',
 			'core-embed/ted',
 			'core-embed/videopress',
-		);
+		];
 
-		if ( 'page' === $editor_context->post->post_type ) {
+		//includes all custom p4 patterns @see https://github.com/greenpeace/planet4-plugin-gutenberg-blocks/tree/main/classes/patterns
+		$p4_custom_pattern_blocks = [
+			'planet4-block-templates/deep-dive',
+			'planet4-block-templates/highlighted-cta',
+			'planet4-block-templates/quick-links',
+			'planet4-block-templates/reality-check',
+			'planet4-block-templates/issues',
+			'planet4-block-templates/page-header',
+			'planet4-block-templates/side-image-with-text-and-cta',
+		];
 
-			$allowed_blocks_page = array(
+		if ( $block_editor_context ->post->post_type === 'page' ) {
+
+			$allowed_blocks_page = [
 				// Blocks from planet4-plugin-gutenberg-blocks
 				// @see: https://github.com/greenpeace/planet4-plugin-gutenberg-blocks/blob/master/planet4-gutenberg-blocks.php
 				'planet4-blocks/accordion',
@@ -98,13 +105,24 @@ if ( ! function_exists( 'p4_child_theme_gpn_whitelist_blocks' ) ) {
 				'acf/leads-form',
 				// 'planet4-blocks/share-buttons', //beta block included
 				'gravityforms/form', // TODO: Leads gen connect to our DB; Gravity Forms block quiz, Email to target, etc.
-			);
-			return array_merge( $allowed_blocks_general, $allowed_blocks_page );
+			];
+
+			//Custom P4 Layouts
+			//@see https://github.com/greenpeace/planet4-plugin-gutenberg-blocks/tree/main/classes/patterns
+			$allowed_p4_layouts_page = [
+				'p4/high-level-topic-pattern-layout',
+				'p4/deep-dive-topic-pattern-layout',
+				'p4/homepage-pattern-layout',
+				'p4/get-informed-pattern-layout',
+				'p4/take-action-pattern-layout',
+			];
+
+			return array_merge( $allowed_blocks_general, $allowed_blocks_page, $p4_custom_pattern_blocks, $allowed_p4_layouts_page );
 		}
 
-		if ( 'post' === $editor_context->post->post_type ) {
+		if ( $block_editor_context ->post->post_type === 'post' ) {
 
-			$allowed_blocks_post = array(
+			$allowed_blocks_post = [
 				// Blocks from planet4-plugin-gutenberg-blocks
 				// @see: https://github.com/greenpeace/planet4-plugin-gutenberg-blocks/blob/master/planet4-gutenberg-blocks.php
 				'planet4-blocks/accordion',
@@ -117,14 +135,49 @@ if ( ! function_exists( 'p4_child_theme_gpn_whitelist_blocks' ) ) {
 				'planet4-blocks/timeline',
 				// 'acf/p4-gpn-block-testimonial',
 				// 'acf/leads-form', //TODO: fix issues on posts
-				'gravityforms/form', // TODO: Leads gen connect to our DB; Gravity Forms block quiz, Email to target, etc.
-			);
+				//'gravityforms/form', // TODO: Leads gen connect to our DB; Gravity Forms block quiz, Email to target, etc.
+			];
 			return array_merge( $allowed_blocks_general, $allowed_blocks_post );
 		}
 
-		if ( 'campaign' === $editor_context->post->post_type ) {
+		if ( $block_editor_context ->post->post_type === 'campaign') {
 
-			$allowed_blocks_campaign = array(
+			$allowed_blocks_campaign = [
+				// Blocks from planet4-plugin-gutenberg-blocks
+				// @see: https://github.com/greenpeace/planet4-plugin-gutenberg-blocks/blob/master/planet4-gutenberg-blocks.php
+				'planet4-blocks/accordion',
+				'planet4-blocks/articles',
+				'planet4-blocks/carousel-header',
+				'planet4-blocks/columns',
+				'planet4-blocks/cookies',
+				'planet4-blocks/counter',
+				'planet4-blocks/covers',
+				'planet4-blocks/gallery',
+				'planet4-blocks/happypoint',
+				'planet4-blocks/media-video',
+				'planet4-blocks/social-media',
+				//'planet4-blocks/split-two-columns',
+				'planet4-blocks/spreadsheet',
+				'planet4-blocks/submenu', //extra
+				'planet4-blocks/sub-pages',
+				'planet4-blocks/timeline',
+				'planet4-blocks/guestbook',
+				// 'acf/p4-gpn-block-testimonial',
+				'acf/leads-form',
+				// 'planet4-blocks/share-buttons', //beta block included
+				'gravityforms/form', // TODO: Leads gen connect to our DB; Gravity Forms block quiz, Email to target, etc.
+			];
+
+			$allowed_p4_layouts_campaign = [
+				'p4/campaign-pattern-layout',
+			];
+
+			return array_merge( $allowed_blocks_general, $allowed_blocks_campaign, $p4_custom_pattern_blocks, $allowed_p4_layouts_campaign );
+		}
+
+		if ( $block_editor_context ->post->post_type === 'action' ) {
+
+			$allowed_blocks_action = [
 				// Blocks from planet4-plugin-gutenberg-blocks
 				// @see: https://github.com/greenpeace/planet4-plugin-gutenberg-blocks/blob/master/planet4-gutenberg-blocks.php
 				'planet4-blocks/accordion',
@@ -147,74 +200,18 @@ if ( ! function_exists( 'p4_child_theme_gpn_whitelist_blocks' ) ) {
 				'acf/leads-form',
 				// 'planet4-blocks/share-buttons', //beta block included
 				'gravityforms/form', // TODO: Leads gen connect to our DB; Gravity Forms block quiz, Email to target, etc.
-			);
-			return array_merge( $allowed_blocks_general, $allowed_blocks_campaign );
+			];
+
+			$allowed_p4_layouts_action = [
+				'p4/campaign-pattern-layout',
+			];
+
+			return array_merge( $allowed_blocks_general, $allowed_blocks_action, $p4_custom_pattern_blocks, $allowed_p4_layouts_action);
 		}
 
-		if ( 'action' === $editor_context->post->post_type ) {
-
-			$allowed_blocks_action = array(
-				// Blocks from planet4-plugin-gutenberg-blocks
-				// @see: https://github.com/greenpeace/planet4-plugin-gutenberg-blocks/blob/master/planet4-gutenberg-blocks.php
-				'planet4-blocks/accordion',
-				'planet4-blocks/articles',
-				'planet4-blocks/carousel-header',
-				'planet4-blocks/columns',
-				'planet4-blocks/cookies',
-				'planet4-blocks/counter',
-				'planet4-blocks/covers',
-				'planet4-blocks/gallery',
-				'planet4-blocks/happypoint',
-				'planet4-blocks/media-video',
-				'planet4-blocks/social-media',
-				'planet4-blocks/split-two-columns',
-				'planet4-blocks/spreadsheet',
-				'planet4-blocks/submenu',
-				'planet4-blocks/timeline',
-				'planet4-blocks/guestbook',
-				// 'acf/p4-gpn-block-testimonial',
-				'acf/leads-form',
-				// 'planet4-blocks/share-buttons', //beta block included
-				'gravityforms/form', // TODO: Leads gen connect to our DB; Gravity Forms block quiz, Email to target, etc.
-			);
-			return array_merge( $allowed_blocks_general, $allowed_blocks_action );
-		}
 		return $blocks;
 	}
-	add_filter( 'allowed_block_types_all', 'p4_child_theme_gpn_whitelist_blocks', 10, 2 );
+
+	add_filter( 'allowed_block_types_all', 'p4_child_theme_gpn_allowed_list', 10, 2 );
 }
 
-if ( ! function_exists( 'p4_child_theme_gpn_whitelist_block_patterns' ) ) {
-	add_filter( 'register_block_pattern_category', 'p4_child_theme_gpn_whitelist_block_patterns', 10, 2 );
-
-	function p4_child_theme_gpn_whitelist_block_patterns( $register_block_pattern_category, $post ) {
-		$register_block_pattern_category_general = array(
-			'planet4-blocks-backend/planet4',
-			'planet4-blocks-backend/planet4-headers',
-			'planet4-blocks-backend/layouts',
-		);
-
-		if ( $post->post_type === 'page' ) { // Block patterns only for pages
-			$register_block_pattern_category_page = array(
-				'p4/high-level-topic-pattern-layout',
-				'p4/deep-dive-topic-pattern-layout',
-				'p4/homepage-pattern-layout',
-				'p4/get-informed-pattern-layout',
-
-			);
-
-			$register_block_pattern_category = array_merge( $register_block_pattern_category_general, $register_block_pattern_category_page );
-		} else if ( $post->post_type === 'campaign' ) { // block patterns only for campaign pages
-			$register_block_pattern_category = array(
-				'p4/campaign-pattern-layout',
-			);
-
-		} else if ( $post->post_type === 'action' ) { // block patterns only for action type pages
-			$register_block_pattern_category = array(
-				'p4/action-pattern-layout',
-			);
-
-		}
-		return $register_block_pattern_category;
-	}
-}
