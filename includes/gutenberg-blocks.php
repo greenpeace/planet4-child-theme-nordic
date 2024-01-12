cb947b9887c75a75442ec9cd75b3dae70059eca9
Svilena GK <svilena_koleva@yahoo.com>
2024-01-12 12:50:45 +0100

    fix: show custom p4 block patterns list in editor

--- includes/gutenberg-blocks.php
+++ includes/gutenberg-blocks.php
@@ -10,6 +10,9 @@
  *
  */
 
+ namespace P4GBKS\Patterns;
+ use P4GBKS\Search\Pattern\PatternData;
+
 if ( ! function_exists( 'p4_child_theme_gpn_allowed_list' ) ) {
 	/**
 	 * Whitelists Gutenberg Blocks
@@ -21,6 +24,7 @@ if ( ! function_exists( 'p4_child_theme_gpn_allowed_list' ) ) {
 	 */
 
 	function p4_child_theme_gpn_allowed_list( $allowed_block_types, $block_editor_context ) {
+
 		$allowed_blocks_general = [
 			'core/block',
 			'core/paragraph',
@@ -80,7 +84,7 @@ if ( ! function_exists( 'p4_child_theme_gpn_allowed_list' ) ) {
 			'planet4-block-templates/side-image-with-text-and-cta',
 		];
 
-		if ( $block_editor_context ->post->post_type === 'page' ) {
+		if ( $block_editor_context->post->post_type === 'page' ) {
 
 			$allowed_blocks_page = [
 				// Blocks from planet4-plugin-gutenberg-blocks
@@ -96,31 +100,43 @@ if ( ! function_exists( 'p4_child_theme_gpn_allowed_list' ) ) {
 				'planet4-blocks/happypoint',
 				'planet4-blocks/media-video',
 				'planet4-blocks/social-media',
-				'planet4-blocks/split-two-columns',
 				'planet4-blocks/spreadsheet',
 				'planet4-blocks/submenu',
 				'planet4-blocks/timeline',
 				'planet4-blocks/guestbook',
 				// 'acf/p4-gpn-block-testimonial',
 				'acf/leads-form',
-				// 'planet4-blocks/share-buttons', //beta block included
+				'planet4-blocks/social-media-cards',//beta block included
+				'planet4-blocks/share-buttons',//beta block included
 				'gravityforms/form', // TODO: Leads gen connect to our DB; Gravity Forms block quiz, Email to target, etc.
 			];
 
 			//Custom P4 Layouts
 			//@see https://github.com/greenpeace/planet4-plugin-gutenberg-blocks/tree/main/classes/patterns
-			$allowed_p4_layouts_page = [
-				'p4/high-level-topic-pattern-layout',
-				'p4/deep-dive-topic-pattern-layout',
-				'p4/homepage-pattern-layout',
-				'p4/get-informed-pattern-layout',
-				'p4/take-action-pattern-layout',
+			$allowed_p4_templates = [
+				'planet4-block-templates/deep-dive',
+				'planet4-block-templates/highlighted-cta',
+				'planet4-block-templates/quick-links',
+				'planet4-block-templates/reality-check',
+				'planet4-block-templates/issues',
+				'planet4-block-templates/page-header',
+				'planet4-block-templates/side-image-with-text-and-cta',
+			
+				// layouts.
+				'planet4-block-templates/deep-dive-topic',
+				'planet4-block-templates/homepage',
+				'planet4-block-templates/campaign',
+				'planet4-block-templates/take-action',
+				'planet4-block-templates/action',
+				'planet4-block-templates/get-informed',
+				'planet4-block-templates/high-level-topic',
 			];
 
-			return array_merge( $allowed_blocks_general, $allowed_blocks_page, $p4_custom_pattern_blocks, $allowed_p4_layouts_page );
+			error_log('Allowed Page Layouts: ' . print_r($allowed_p4_templates, true));
+			return array_merge( $allowed_blocks_general, $allowed_blocks_page, $p4_custom_pattern_blocks, $allowed_p4_templates );
 		}
 
-		if ( $block_editor_context ->post->post_type === 'post' ) {
+		if ( $block_editor_context->post->post_type === 'post' ) {
 
 			$allowed_blocks_post = [
 				// Blocks from planet4-plugin-gutenberg-blocks
@@ -134,48 +150,15 @@ if ( ! function_exists( 'p4_child_theme_gpn_allowed_list' ) ) {
 				'planet4-blocks/take-action-boxout',
 				'planet4-blocks/timeline',
 				// 'acf/p4-gpn-block-testimonial',
-				//'acf/leads-form', //TODO: fix issues on posts
+				'acf/leads-form', //TODO: fix issues on posts
 				//'gravityforms/form', // TODO: Leads gen connect to our DB; Gravity Forms block quiz, Email to target, etc.
 			];
 			return array_merge( $allowed_blocks_general, $allowed_blocks_post );
 		}
 
-		if ( $block_editor_context ->post->post_type === 'campaign') {
-
-			$allowed_blocks_campaign = [
-				// Blocks from planet4-plugin-gutenberg-blocks
-				// @see: https://github.com/greenpeace/planet4-plugin-gutenberg-blocks/blob/master/planet4-gutenberg-blocks.php
-				'planet4-blocks/accordion',
-				'planet4-blocks/articles',
-				'planet4-blocks/carousel-header',
-				'planet4-blocks/columns',
-				'planet4-blocks/cookies',
-				'planet4-blocks/counter',
-				'planet4-blocks/covers',
-				'planet4-blocks/gallery',
-				'planet4-blocks/happypoint',
-				'planet4-blocks/media-video',
-				'planet4-blocks/social-media',
-				//'planet4-blocks/split-two-columns',
-				'planet4-blocks/spreadsheet',
-				'planet4-blocks/submenu', //extra
-				'planet4-blocks/sub-pages',
-				'planet4-blocks/timeline',
-				'planet4-blocks/guestbook',
-				// 'acf/p4-gpn-block-testimonial',
-				'acf/leads-form',
-				// 'planet4-blocks/share-buttons', //beta block included
-				'gravityforms/form', // TODO: Leads gen connect to our DB; Gravity Forms block quiz, Email to target, etc.
-			];
-
-			$allowed_p4_layouts_campaign = [
-				'p4/campaign-pattern-layout',
-			];
-
-			return array_merge( $allowed_blocks_general, $allowed_blocks_campaign, $p4_custom_pattern_blocks, $allowed_p4_layouts_campaign );
-		}
-
-		if ( $block_editor_context ->post->post_type === 'action' ) {
+		//removed "campaign" logic as it will be decomissioned soon
+		
+		if ( $block_editor_context->post->post_type === 'action' ) {
 
 			$allowed_blocks_action = [
 				// Blocks from planet4-plugin-gutenberg-blocks
@@ -191,27 +174,30 @@ if ( ! function_exists( 'p4_child_theme_gpn_allowed_list' ) ) {
 				'planet4-blocks/happypoint',
 				'planet4-blocks/media-video',
 				'planet4-blocks/social-media',
-				'planet4-blocks/split-two-columns',
 				'planet4-blocks/spreadsheet',
 				'planet4-blocks/submenu',
 				'planet4-blocks/timeline',
 				'planet4-blocks/guestbook',
+				'gravityforms/form',
+				'planet4-blocks/sub-pages',
 				// 'acf/p4-gpn-block-testimonial',
 				'acf/leads-form',
-				// 'planet4-blocks/share-buttons', //beta block included
 				'gravityforms/form', // TODO: Leads gen connect to our DB; Gravity Forms block quiz, Email to target, etc.
+				'planet4-blocks/social-media-cards',//beta block included
+				'planet4-blocks/share-buttons',//beta block included
 			];
 
 			$allowed_p4_layouts_action = [
 				'p4/campaign-pattern-layout',
 			];
 
+			error_log('Allowed Action Layouts: ' . print_r($allowed_p4_layouts_action, true));
 			return array_merge( $allowed_blocks_general, $allowed_blocks_action, $p4_custom_pattern_blocks, $allowed_p4_layouts_action);
 		}
 
-		return $blocks;
+		return $allowed_block_types;
 	}
 
-	add_filter( 'allowed_block_types_all', 'p4_child_theme_gpn_allowed_list', 10, 2 );
+	add_filter( 'allowed_block_types_all', 'P4GBKS\Patterns\p4_child_theme_gpn_allowed_list', 10, 2 );
 }
 
