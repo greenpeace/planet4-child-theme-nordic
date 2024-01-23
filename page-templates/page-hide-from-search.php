a83f6c0d474570ad14896a1d3b0e3c32bd4f7a1e
Svilena GK <svilena_koleva@yahoo.com>
2024-01-23 12:37:49 +0100

    fix: hide page on search template breaks layout

--- page-templates/page-hide-from-search.php
+++ page-templates/page-hide-from-search.php
@@ -11,24 +11,27 @@ Template Name: Hide page from search results (GPN)
 
 
 // Example:
-// www.greenepace.org/country/something/page-hide-from-serach.php
+// www.greenpeace.org/country/something/page-hide-from-serach.php
 
-use P4\MasterTheme\Context;
 use P4\MasterTheme\Post;
+use P4\MasterTheme\Context;
 use Timber\Timber;
 
-$context        = Timber::get_context();
-$post           = new Post(); // phpcs:ignore WordPress.WP.GlobalVariablesOverride.Prohibited
-//update the current page template meta to not show in the page list in the search results
-add_post_meta( $post->ID, 'p4_do_not_index', true );
-$page_meta_data = get_post_meta( $post->ID );
-//get the content of the current page
-$context['post'] = $post;
-// $context['page_category']       = 'Hidden Page';
+if ( defined( 'ABSPATH' ) && function_exists( 'add_action' ) ) {
+    $context = Timber::get_context();
+    $post = new Post(); // phpcs:ignore WordPress.WP.GlobalVariablesOverride.Prohibited
+    $page_meta_data = get_post_meta($post->ID);
+    $page_meta_data = array_map(fn ($v) => reset($v), $page_meta_data);
+    
+    //update the current page template meta to not show in the page list in the search results
+    add_post_meta( $post->ID, 'p4_do_not_index', true );
+    //get the content of the current page
+    $context['post'] = $post;
+    $context['page_category']       = 'Hide page from search';
 
-Context::set_header( $context, $page_meta_data, $post->title );
-Timber::render( 'page.twig', $context );
-
+    Context::set_header( $context, $page_meta_data, $post->title );
+    Timber::render( 'page-hide-from-search.twig', $context );
+}
 ?>
 
 
