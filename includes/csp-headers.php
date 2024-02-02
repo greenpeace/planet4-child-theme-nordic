1d3cf429608e19d78e722c6e5f3f2ed02082e30b
Svilena GK <svilena_koleva@yahoo.com>
2024-02-02 10:33:55 +0100

    test: modify header to whitelist frame src

--- includes/csp-headers.php
+++ includes/csp-headers.php
@@ -5,9 +5,15 @@
  */
 
 add_action('wp_headers', function ($headers): array {
-    if (empty($headers['Content-Security-Policy'])) {
-        return $headers;
+    // Modify Content-Security-Policy
+    if (isset($headers['Content-Security-Policy'])) {
+        $headers['Content-Security-Policy'] .= "; worker-src 'self' blob: * data: *; frame-ancestors 'self' www.greenpeace.rocks;";
     }
-    $headers['Content-Security-Policy'] .= "; worker-src 'self' blob: * data: *; frame-ancestors 'self' www.greenpeace.rocks;";
+
+    // Modify X-Frame-Options
+    if (isset($headers['X-Frame-Options']) && $headers['X-Frame-Options'] === 'SAMEORIGIN') {
+        // Change SAMEORIGIN to whatever is needed, e.g., 'ALLOW-FROM https://www.greenpeace.rocks/'
+        $headers['X-Frame-Options'] = 'ALLOW-FROM https://www.greenpeace.rocks/';
+    }    
     return $headers;
 }, 11, 1);
