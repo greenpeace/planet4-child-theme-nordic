929a1d5f656bb30b36b2fe8e951eb47f9f4585d4
elle <svilena_koleva@yahoo.com>
2025-02-24 16:57:17 +0100

    Version up
    Fixed typo, removed log mssgs

--- assets/src/js/app.js
+++ assets/src/js/app.js
@@ -18,21 +18,21 @@ import './frontend/nosearch';
 import './frontend/templates';
 import './frontend/tracking';
 
-// Function to check if window.wp is ready
-const waitForWP = (callback) => {
-    if (window.wp) {
-        callback();
-    } else {
-        console.warn("window.wp is not defined. Retrying...");
-        setTimeout(() => waitForWP(callback), 100); // Check every 100ms
-    }
-};
+// // Function to check if window.wp is ready
+// const waitForWP = (callback) => {
+//     if (window.wp) {
+//         callback();
+//     } else {
+//         console.warn("window.wp is not defined. Retrying...");
+//         setTimeout(() => waitForWP(callback), 100); // Check every 100ms
+//     }
+// };
 
-// Run when DOM is loaded
-document.addEventListener("DOMContentLoaded", () => {
-    waitForWP(() => {
-        console.log("✅ WordPress is loaded:", window.wp);
-        console.log("wp.blocks:", window.wp.blocks);
-        console.log("wp.data:", window.wp.data);
-    });
-});
+// // Run when DOM is loaded
+// document.addEventListener("DOMContentLoaded", () => {
+//     waitForWP(() => {
+//         console.log("✅ WordPress is loaded:", window.wp);
+//         console.log("wp.blocks:", window.wp.blocks);
+//         console.log("wp.data:", window.wp.data);
+//     });
+// });
