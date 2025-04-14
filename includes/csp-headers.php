fcae50ed1f6d64096a3c415f030b822113380d91
elle <svilena_koleva@yahoo.com>
2025-04-14 16:10:39 +0200

    chore: modify the CSP headers to keep mt set up and add the convert

--- includes/csp-headers.php
+++ includes/csp-headers.php
@@ -4,12 +4,11 @@
  * Update the CSP rules and allow the load of blob objects
  */
 
- add_action('wp_headers', function ($headers): array {
-    $headers['Content-Security-Policy'] = implode(' ', [
-        "default-src 'self';",
-        "connect-src 'self' blob: data: https://app.convert.com;",
-        "worker-src 'self' blob:;",
-        "frame-ancestors 'self';"
-    ]);
+add_action('wp_headers', function ($headers): array {
+    // Modify Content-Security-Policy
+    if (isset($headers['Content-Security-Policy'])) {
+        //whitelist the app.convert.com domain
+        $headers['Content-Security-Policy'] .= " connect-src 'self' blob: data: https://app.convert.com;";
+    }
     return $headers;
 }, 11, 1);
