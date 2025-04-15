27c86f6dd453a6a462c3195cf1cc8cb6774c48b9
elle <svilena_koleva@yahoo.com>
2025-04-15 13:05:16 +0200

    test: update cph header

--- includes/csp-headers.php
+++ includes/csp-headers.php
@@ -6,7 +6,7 @@
 
 add_action('wp_headers', function ($headers): array {
 
-    $headers['Content-Security-Policy'] .= "; worker-src 'self'; connect-src 'self' convert.com app.convert.com convertexperiments.com cdn-4.convertexperiments.com";
-
+    $headers['Content-Security-Policy'] .= "; worker-src 'self' blob: * data: *; connect-src 'self' https://convert.com https://app.convert.com https://convertexperiments.com https://cdn-4.convertexperiments.com";
     return $headers;
+    
 }, 11, 1);
