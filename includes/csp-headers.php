ec184c530e2cbe7b20a51259b3a6e1d1fc06d8ad
elle <svilena_koleva@yahoo.com>
2025-04-15 11:26:55 +0200

    test: update csp header

--- includes/csp-headers.php
+++ includes/csp-headers.php
@@ -5,22 +5,8 @@
  */
 
 add_action('wp_headers', function ($headers): array {
-    $new_sources = [
-        "https://convert.com",
-        "https://app.convert.com",
-        "https://convertexperiments.com",
-        "https://cdn-4.convertexperiments.com"
-    ];
 
-    if (isset($headers['Content-Security-Policy'])) {
-        // No connect-src found, add it safely and inclue what's already whitelisted
-        if (strpos($headers['Content-Security-Policy'], 'connect-src') === false) {
-            $headers['Content-Security-Policy'] .= '; connect-src ' . implode(' ', $new_sources) . ';';
-        } else {
-            // If connect-src is already present, replace it with the new sources
-            $headers['Content-Security-Policy'] = preg_replace('/connect-src [^;]+/', 'connect-src ' . implode(' ', $new_sources), $headers['Content-Security-Policy']);
-        }
-    }
+    $headers['Content-Security-Policy'] .= "; worker-src 'self'; connect-src 'self' convert.com app.convert.com convertexperiments.com cdn-4.convertexperiments.com";
 
     return $headers;
 }, 11, 1);
