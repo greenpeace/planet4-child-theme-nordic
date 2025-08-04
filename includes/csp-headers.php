f6dae98ac16df740811a1ccf9efa7d9b77310c72
elle <svilena_koleva@yahoo.com>
2025-08-04 16:19:35 +0200

    fix: loosen a bit the csp policy to not block some of our files
    This update broadens the allowed sources for scripts, styles, fonts, images, connections, and frames, while maintaining a strong focus on 'self' origin and secure protocols. This ensures compatibility with necessary third-party resources, such as CDNs and external services, without compromising overall security posture.
    
    It also introduces `X-Content-Security-Policy` for backward compatibility.

--- includes/csp-headers.php
+++ includes/csp-headers.php
@@ -5,26 +5,7 @@
  */
 
 add_action('wp_headers', function ($headers): array {
-
-    $headers['Content-Security-Policy'] = "
-        default-src 'self';
-        script-src 'self' 'unsafe-inline' 'unsafe-eval'
-            https://*.iraiser.eu https://*.convert.com https://*.greenpeace.org https://*.fontawesome.com;
-        style-src 'self' 'unsafe-inline' https://*.greenpeace.org https://*.convert.com https://*.fontawesome.com;
-        img-src 'self' data: blob: https://*.greenpeace.org https://*.convert.com https://*.iraiser.eu;
-        font-src 'self' data: https://*.fontawesome.com https://*.convert.com;
-        connect-src 'self'
-            https://*.convert.com
-            https://*.iraiser.eu
-            https://*.greenpeace.org
-            https:;
-        frame-src
-            https://*.iraiser.eu
-            https://*.convert.com
-            https://*.greenpeace.org
-            https:;
-        frame-ancestors 'self';
-";
+    $headers['Content-Security-Policy'] = "default-src 'self' data: blob: *; script-src 'self' 'unsafe-inline' 'unsafe-eval' data: blob: https://*.greenpeace.org https://*.convert.com https://*.convertexperiments.com http://*.convertexperiments.com https://*.iraiser.com https://*.fontawesome.com https://cdnjs.cloudflare.com; style-src 'self' 'unsafe-inline' https://fonts.googleapis.com https://*.greenpeace.org https://*.convert.com https://*.fontawesome.com; font-src 'self' data: https://fonts.gstatic.com https://*.fontawesome.com https://*.greenpeace.org; img-src 'self' data: blob: https://*.greenpeace.org https://*.convert.com https://*.iraiser.com https://cdnjs.cloudflare.com https://*.gravatar.com https://lh3.googleusercontent.com; connect-src 'self' https: http: wss: ws: https://*.convert.com https://*.iraiser.com https://*.greenpeace.org; frame-src 'self' https://*.convert.com https://*.convertexperiments.com http://*.convertexperiments.com https://*.iraiser.com https://*.greenpeace.org; frame-ancestors 'self';";
     $headers['X-Content-Security-Policy'] = $headers['Content-Security-Policy'];
     return $headers;
 }, 11, 1);
