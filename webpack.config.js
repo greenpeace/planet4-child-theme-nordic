339801c43c7c6678fb70ccc6640a5d748d60df3d
elle <svilena_koleva@yahoo.com>
2025-08-05 09:56:51 +0200

    fix: Updates CSP headers and build process
    Refactors CSP headers to allow inline scripts and styles.
    
    Updates the build process to use development mode during start for better debugging.
    
    Refactors the TA Cover Card to inherit border-radius from the action list.

--- webpack.config.js
+++ webpack.config.js
@@ -8,133 +8,137 @@ const DependencyExtractionWebpackPlugin = require('@wordpress/dependency-extract
 const webpack = require('webpack');
 const ProvidePlugin = require('buffer');
 
-module.exports = {
-    // ...defaultConfig,
-    entry: {
-        index: './assets/src/js/app.js',
-    },
-    output: {
-        filename: '[name].js',
-        path: __dirname + '/assets/build'
-    },
-    module: {
-        // ...defaultConfig.module,
-        rules: [
-            {
-                test: /\.(sass|scss)$/,
-                use: [
-                    MiniCssExtractPlugin.loader,
-                    {
-                        loader: 'css-loader',
-                        options: {
-                            url: false,
-                            sourceMap: true,
-                        }
-                    },
-                    {
-                        loader: 'postcss-loader',
-                        options: {
-                            postcssOptions: {
-                                plugins: [
-                                    require('autoprefixer'),
-                                ],
-                            },
-                            sourceMap: true,
-                        },
-                    },
-                    {
-                        loader: 'sass-loader',
-                        options: {
-                            sourceMap: true,
-                        }
-                    }
-                ]
-            },
-            {
-                test: /icons\/.*\.svg$/,
-                loader: 'svg-sprite-loader',
-                options: {
-                    extract: true,
-                    spriteFilename: '../../images/symbol/svg/sprite.symbol.svg',
-                    runtimeCompat: true
-                }
-            },
-        ]
-    },
-    plugins: [
-        new webpack.ProvidePlugin({
-            Buffer: ['buffer', 'Buffer'],
-            $: 'jquery',
-            jQuery: 'jquery',
-        }),
-        new MiniCssExtractPlugin({
-            filename: 'style.min.css',
-            chunkFilename: '[id].min.css',
-            ignoreOrder: false,
-        }),
-        new RemovePlugin({
-            after: {
-                test: [{
-                    folder: 'assets/build/',
-                    method: (filePath) => {
-                        return [
-                            'style.deps.json',
-                            'index.asset.php',
-                            'style.asset.php',
-                        ].some(item => new RegExp(item, 'm').test(filePath));
-                    }
-                }]
-            }
-        }),
-        new SpriteLoaderPlugin({
-            plainSprite: true
-        }),
-        new DependencyExtractionWebpackPlugin(),
-    ],
-    optimization: {
-        splitChunks: {
-            cacheGroups: {
-                styles: {
-                    name: 'styles',
-                    test: /\.css$/,
-                    chunks: 'all',
-                    enforce: true,
-                },
-                default: {
-                    name: 'default',
-                    minChunks: 2,
-                    priority: -20,
-                    reuseExistingChunk: true,
-                    enforce: true,
-                },
-            },
+module.exports = (env, argv) => {
+    const isProduction = argv.mode === 'production';
+    return {
+        // ...defaultConfig,
+        entry: {
+            index: './assets/src/js/app.js',
         },
-        minimizer: [
-            new TerserJSPlugin({}),
-            new CssMinimizerPlugin({
-                minimizerOptions: {
-                    preset: [
-                        'default',
+        output: {
+            filename: '[name].js',
+            path: __dirname + '/assets/build'
+        },
+        module: {
+            // ...defaultConfig.module,
+            rules: [
+                {
+                    test: /\.(sass|scss)$/,
+                    use: [
+                        MiniCssExtractPlugin.loader,
                         {
-                            discardComments: { removeAll: true },
-                            sourceMap: true,
+                            loader: 'css-loader',
+                            options: {
+                                url: false,
+                                sourceMap: true,
+                            }
                         },
-                    ],
+                        {
+                            loader: 'postcss-loader',
+                            options: {
+                                postcssOptions: {
+                                    plugins: [
+                                        require('autoprefixer'),
+                                    ],
+                                },
+                                sourceMap: true,
+                            },
+                        },
+                        {
+                            loader: 'sass-loader',
+                            options: {
+                                sourceMap: true,
+                            }
+                        }
+                    ]
                 },
+                {
+                    test: /icons\/.*\.svg$/,
+                    loader: 'svg-sprite-loader',
+                    options: {
+                        extract: true,
+                        spriteFilename: '../../images/symbol/svg/sprite.symbol.svg',
+                        runtimeCompat: true
+                    }
+                },
+            ]
+        },
+        plugins: [
+            new webpack.ProvidePlugin({
+                Buffer: ['buffer', 'Buffer'],
+                $: 'jquery',
+                jQuery: 'jquery',
             }),
+            new MiniCssExtractPlugin({
+                filename: 'style.min.css',
+                chunkFilename: '[id].min.css',
+                ignoreOrder: false,
+            }),
+            new RemovePlugin({
+                after: {
+                    test: [{
+                        folder: 'assets/build/',
+                        method: (filePath) => {
+                            return [
+                                'style.deps.json',
+                                'index.asset.php',
+                                'style.asset.php',
+                            ].some(item => new RegExp(item, 'm').test(filePath));
+                        }
+                    }]
+                }
+            }),
+            new SpriteLoaderPlugin({
+                plainSprite: true
+            }),
+            new DependencyExtractionWebpackPlugin(),
         ],
-    },
-    resolve: {
-        fallback: {
-            buffer: require.resolve('buffer/'),
+        optimization: {
+            splitChunks: {
+                cacheGroups: {
+                    styles: {
+                        name: 'styles',
+                        test: /\.css$/,
+                        chunks: 'all',
+                        enforce: true,
+                    },
+                    default: {
+                        name: 'default',
+                        minChunks: 2,
+                        priority: -20,
+                        reuseExistingChunk: true,
+                        enforce: true,
+                    },
+                },
+            },
+            minimizer: [
+                new TerserJSPlugin({}),
+                new CssMinimizerPlugin({
+                    minimizerOptions: {
+                        preset: [
+                            'default',
+                            {
+                                discardComments: { removeAll: true },
+                                sourceMap: true,
+                            },
+                        ],
+                    },
+                }),
+            ],
         },
-        extensions: ['.js', '.scss'],
-        alias: {
-            jquery: 'jquery/src/jquery'
+        resolve: {
+            fallback: {
+                buffer: require.resolve('buffer/'),
+            },
+            extensions: ['.js', '.scss'],
+            alias: {
+                jquery: 'jquery/src/jquery'
+            },
         },
-    },
-    devtool: 'source-map',
-    stats: {
-        all: true,
-    },
+        devtool: isProduction ? false : 'source-map',
+        stats: {
+            all: true,
+        },
+
+    }
 };
