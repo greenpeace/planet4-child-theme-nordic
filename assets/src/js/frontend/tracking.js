73d5d0e8639f95b0d9a00720e243a1d87ee0f1d9
elle <svilena_koleva@yahoo.com>
2025-11-28 15:51:04 +0100

    fix: move the A/b test trigger into a click event

--- assets/src/js/frontend/tracking.js
+++ assets/src/js/frontend/tracking.js
@@ -1,15 +1,17 @@
 //tracking the sub-menu block link clicks
 window.addEventListener('DOMContentLoaded', (event) => {
 
-  //adds the script to trigger conversion tracking A/B test in DK {Support us btn}
-  window._conv_q = window._conv_q || [];
-  window._conv_q.push({
-    what: "triggerLocation",
-    params: {
-      locationId: "1004138120"
-    }
+  jQuery("#nav-main .btn-donate").on('click', function () {
+    //adds the script to trigger conversion tracking A/B test in DK {Support us btn}
+    window._conv_q = window._conv_q || [];
+    window._conv_q.push({
+      what: "triggerLocation",
+      params: {
+        locationId: "1004138120"
+      }
+    });
+    console.log(window._conv_q);
   });
-  // console.log(window._conv_q);
 
   jQuery('.submenu-link').on('click', function (e) {
     let submenuLinkHref = jQuery(this).attr('href');
