7fa3e4e5e04f2664543a8f3fe0743c174572698f
elle <svilena_koleva@yahoo.com>
2026-08-13 09:56:50 +0200

    fix: ensure the latest version of the page w LP is served by CF

--- functions.php
+++ functions.php
@@ -462,3 +462,13 @@ function Get_All_Hidden_Template_pages()
     $hiddenTemplatePages = $query->posts;
     return $hiddenTemplatePages;
 }
+
+/**
+ * Tell visitor browsers to check Cloudflare for the latest version on every load,
+ * while inheriting all default Cloudflare CDN and "Always Online" rules.
+ */
+add_action('send_headers', function () {
+    if (!is_admin() && !is_user_logged_in()) {
+        header('Cache-Control: no-cache');
+    }
+});
